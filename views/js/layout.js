e41a06efd10673c24cc36d7ec2280cb48fbbdd61
Ali_Alaa_Elidn <alialaa932003@gmail.com>
2023-11-22 16:27:37 +0200

    s

---
+++ views/js/layout.js
@@ -0,0 +1,230 @@
+let mainNavbar = `<div class="parent">
+    <div class="top-bar">
+        <div class="container-fluid">
+            <div class="box">
+                <div class="right-box">
+                    <div class="item">
+                        <span>20123456789+</span>
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                    </div>
+                    <div class="item">
+                        <span>example123@gmail.com</span>
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div class="left-box">
+                    <div class="item">
+                        <span>تتبع شحنتك</span>
+                        <i class="fa-solid fa-truck-plane"></i>
+                    </div>
+                    <div class="item">
+                        <span>أماكن الفروع</span>
+                        <i class="fa fa-location-dot" aria-hidden="true"></i>
+                    </div>
+                    <div class="features">
+                        <div class="item">
+                            <span>العربية</span>
+                            <i
+                                    class="fa-solid fa-sort-down abs"
+                                    aria-hidden="true"
+                            ></i>
+                        </div>
+                        <div class="item">
+                            <i
+                                    class="fa-solid fa-sort-down abs"
+                                    aria-hidden="true"
+                            ></i>
+
+                            <span>EGP</span>
+                            <i class="fa-solid fa-coins"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light nav2">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="home1.html"
+            ><img src="img/logo.png" alt="" />
+                <span>دكان</span></a
+            >
+            <form class="d-flex">
+                <input
+                        class="form-control me-2"
+                        type="search"
+                        placeholder="ابحث هنا"
+                        aria-label="Search"
+                />
+                <!-- <button class="btn btn-outline-success" type="submit">Search</button> -->
+            </form>
+            <div class="nav-icon lol">
+                <span><i class="fa fa-sliders" aria-hidden="true"></i></span>
+                <span class="shopping"
+                ><i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                  </span>
+                <a href="login.html">
+                    تسجيل
+                    <i class="fa fa-user-circle" aria-hidden="true"></i>
+                </a>
+            </div>
+            <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+            >
+                <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="categories.html"
+                        >جميع الفئات</a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html"> الموضة</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html">السوبر ماركت</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html">العطور</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html">موبايلات</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html">الالكترونيات</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html">الاجهزة المنزلية</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html">العاب الكترونية</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html">عروض اليوم</a>
+                    </li>
+                </ul>
+                <div class="nav-icon">
+                     <span
+                     ><i class="fa fa-sliders" aria-hidden="true"></i
+                     ></span>
+                    <span class="shopping"
+                    ><i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                     </span>
+                    <a href="login.html">
+                        تسجيل
+                        <i class="fa fa-user-circle" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+            <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+            >
+                <i class="fa-solid fa-bars"></i>
+            </button>
+        </div>
+    </nav>
+</div>`;
+
+let footer = `<footer class="">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="box">
+                            <div class="brand">
+                                <img src="img/logo.png" alt="" />
+                                <span> دكان </span>
+                            </div>
+                            <p>
+                                موقع دكان للبيع و الشراء اون لاين تصميم فريق
+                                طلاب كلية حاسبات وذكاء اصطناعي جامعة حلوان
+                            </p>
+                            <div class="icons">
+                                <span><i class="fa fa-facebook"></i></span>
+                                <span><i class="fa fa-youtube"></i></span>
+                                <span><i class="fa fa-instagram"></i></span>
+                                <span><i class="fa fa-twitter"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="box-two">
+                            <h3>الفئات</h3>
+                            <ul>
+                                <li><a href="search.html">الالكترونيات</a></li>
+                                <li><a href="search.html">موبايلات</a></li>
+                                <li><a href="search.html">العطور</a></li>
+                                <li><a href="search.html">الموضة</a></li>
+                                <li><a href="search.html">عرض اليوم</a></li>
+                                <li>
+                                    <a href="search.html">العاب الكترونية</a>
+                                </li>
+                                <li><a href="search.html">أجهزة كمبيوتر</a></li>
+                                <li>
+                                    <a href="search.html">مستلزمات منزلية</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="box-two">
+                            <h3>الصفحات</h3>
+                            <ul>
+                                <li><a href="about.html">عن دكان</a></li>
+                                <li><a href="faq.html">سياسات خصوصية</a></li>
+                                <li><a href="blogs.html">المدونة</a></li>
+                                <li><a href="contact.html">تواصل معنا </a></li>
+                                <li><a href="login.html">تسجيل بائع</a></li>
+                                <li>
+                                    <a href="profile.html">الصفحة الشخصية</a>
+                                </li>
+                                <li><a href="wishlist.html">المفضلة</a></li>
+                                <li><a href="cart.html">العربة</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="box">
+                            <h3>يمكنك التواصل مع خدمة عملائنا</h3>
+                            <p>
+                                يمكنك التواصل مع خدمه العملاء بأكثر من طريقة لحل
+                                مشكلتك او تلقي أقتراحك
+                            </p>
+                            <div class="contact-us">
+                                <div class="content">
+                                    <p>تواصل على الخط الساخن</p>
+                                    <p>20123456789+</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="fa fa-headphones-simple"></i>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="download">
+                                    <div>
+                                        <span> حمل التطبيق </span>
+                                        <span>google play</span>
+                                    </div>
+                                    <div>
+                                        <i class="fa-brands fa-google-play"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <div class="copy-right">
+            <p>حقوق الطبع والنشر © لعام 2022 محفوظة لموقع <span>دكان</span></p>
+        </div>`;
+let layout = () => {
+    document.body.insertAdjacentHTML("afterbegin", mainNavbar);
+    document.body.insertAdjacentHTML("beforeend", footer);
+};
+layout();
